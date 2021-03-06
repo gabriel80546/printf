b02f6c4cbc393a99011deb4da314feeafb0331b7
gabriel <gabriel80546@gmail.com>
2021-03-06 11:06:22 -0300

    começando a usar va_list

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 08:29:20 by gabriel           #+#    #+#             */
-/*   Updated: 2021/03/06 10:25:49 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/03/06 11:05:49 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,54 @@ char	*ft_append(char *str, char caracter)
 	return (saida);
 }
 
+char	*ft_strappend(char *str, char *to_append)
+{
+	char	*saida;
+	int		i;
+	int		len_str;
+	int		len_append;
+
+	len_str = ft_strlen(str);
+	len_append = ft_strlen(to_append);
+	saida = (char *)malloc(len_str + len_append + 2);
+	ft_strlcpy(saida, str, len_str + 1);
+	i = 0;
+	while (to_append[i] != '\0')
+	{
+		saida[len_str + i] = to_append[i];
+		i++;
+	}
+	saida[len_str + i] = '\0';
+	free(str);
+	return (saida);
+}
+
 int		ft_printf(const char *str, ...)
 {
 	char	*output;
 	int		i;
 
+    va_list arguments;
+    va_start(arguments, str);
+	// printf("va_arg(arguments, int) = %d\n", va_arg(arguments, int));
+
 	i = 0;
 	output = ft_calloc(1, 1);
 	while (str[i] != '\0')
 	{
-		if(str[i] != '%')
+		// printf("output = '%s'\n", output);
+		if (str[i] != '%')
 			output = ft_append(output, str[i]);
 		else
-			output = ft_append(output, '@');
+		{
+			output = ft_strappend(output, ft_itoa(va_arg(arguments, int)));
+			i++;
+		}
 		i++;
 	}
 	ft_putstr_fd(output, 1);
 	free(output);
+
+    va_end(arguments);
 	return (0);
 }
--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 08:31:12 by gabriel           #+#    #+#             */
-/*   Updated: 2021/03/06 09:52:59 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/03/06 11:06:02 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int main()
 	// char	*p = "'string p'";
 	// int		i = 4;
 	printf   ("numero normal = %d\n", 123);
-	ft_printf("numero normal = %d\n", 123);
+	ft_printf("numero normal = %d\n", 234);
 	// printf("%%%dbada%s%%**%s**-d%%0*d%-12s0*@", h, "bada", r, p, r, r, i, r, i, r, i);
 	// printf("saida = %d\n", ft_printf("Hello World!\n %d\n", 5));
 	return (0);
