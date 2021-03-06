21d4a35e35ffbe843f5cd6c52a1b811bcf065ebc
gabriel <gabriel80546@gmail.com>
2021-03-06 09:56:41 -0300

    pensando em estrategias

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 08:29:20 by gabriel           #+#    #+#             */
-/*   Updated: 2021/03/05 10:26:38 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/03/06 09:56:16 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 
 int		ft_printf(const char *str, ...)
 {
-	int len = ft_strlen(str);
-	return (len);
+	char	*output;
+	int		i;
+	int		len;
+
+	len = ft_strlen(str);
+	output = (char *)malloc(len + 1);
+	i = 0;
+	while (i < len)
+	{
+		if(*(str + i) != '%')
+			*(output + i) = *(str + i);
+		else
+			*(output + i) = '@';
+		i++;
+	}
+	ft_putstr_fd(output, 1);
+	return (0);
 }
--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 08:31:12 by gabriel           #+#    #+#             */
-/*   Updated: 2021/03/05 10:19:33 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/03/06 09:52:59 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,13 @@
 
 int main()
 {
-	printf("Hello World!\n");
-	printf("saida = %d\n", ft_printf("Hello World!\n"));
+	// char	*r = "'string r'";
+	// int		h = 2;
+	// char	*p = "'string p'";
+	// int		i = 4;
+	printf   ("numero normal = %d\n", 123);
+	ft_printf("numero normal = %d\n", 123);
+	// printf("%%%dbada%s%%**%s**-d%%0*d%-12s0*@", h, "bada", r, p, r, r, i, r, i, r, i);
+	// printf("saida = %d\n", ft_printf("Hello World!\n %d\n", 5));
 	return (0);
 }
