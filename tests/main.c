0d7d22b50d455698e1ac2ce7eac5ab39cc1bea8d
gabriel <gabriel80546@gmail.com>
2021-03-11 10:32:25 -0300

    removido logging

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 08:31:12 by gabriel           #+#    #+#             */
-/*   Updated: 2021/03/11 09:42:24 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/03/11 10:16:23 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,36 @@
 
 int main()
 {
+
+
+
+
+	int		a = -4;
+	int		b = 0;
+	char	c = 'a';
+	int		d = 2147483647;
+	int		e = -2147483648;
+	int		f = 42;
+	int		g = 25;
+	int		h = 4200;
+	int		i = 8;
+	int		j = -12;
+	int		k = 123456789;
+	int		l = 0;
+	int		m = -12345678;
+	char	*n = "abcdefghijklmnop";
+	char	*o = "-a";
+	char	*p = "-12";
+	char	*q = "0";
+	char	*r = "%%";
+	char	*s = "-2147483648";
+	char	*t = "0x12345678";
+	char	*u = "-0";
+
+
+
+
+
 	// printf("%1.2d\n", 42);
 	int verbose;
 	verbose = 0;
@@ -48,9 +78,18 @@ int main()
 	printf   ("main: 48: {%10d}\n", 42);
 	ft_printf("main: 49: {%10d}\n", 42);
 
- 	int             d = 2147483647;
 	if (verbose == 1) { printf   ("\n\"main: 51: %%d\\n\", 2147483647\n"); }
 	printf   ("main: 52: %d\n", d);
 	ft_printf("main: 53: %d\n", d);
+
+	// printf("Simple input test"));
+	// printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X", i, j, k, l, m, n, c, c, j, j, j)); //T2
+	if (verbose == 1) { printf   ("\n\"main: 87: %%0i, %%0d, %%0d, %%0d, %%0d, %%0d, %%0d, %%0d\\n\", i, j, k, l, m, c, e, d\n"); } //T3
+	printf   ("main: 88: %0i, %0d, %0d, %0d, %0d, %0d, %0d, %0d\n", i, j, k, l, m, c, e, d); //T3
+	ft_printf("main: 89: %0i, %0d, %0d, %0d, %0d, %0d, %0d, %0d\n", i, j, k, l, m, c, e, d); //T3
+
+	if (verbose == 1) { printf   ("\n\"main: 91: %%1i, %%1d, %%1d, %%1d, %%1d, %%1d, %%1d, %%1d\\n\", i, j, k, l, m, c, e, d\n"); } //T4
+	printf   ("main: 92: %1i, %1d, %1d, %1d, %1d, %1d, %1d, %1d\n", i, j, k, l, m, c, e, d); //T4
+	ft_printf("main: 93: %1i, %1d, %1d, %1d, %1d, %1d, %1d, %1d\n", i, j, k, l, m, c, e, d); //T4
 	return (0);
 }
