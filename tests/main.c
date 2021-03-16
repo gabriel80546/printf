723f73d64954645c5a5e7c4d1559e64c1f543b8e
gabriel <gabriel80546@gmail.com>
2021-03-16 11:49:31 -0300

    testes *

--- tests/main.c
+++ tests/main.c
@@ -24,6 +24,16 @@ int main()
 	verbose = 0;
 	verbose = 1;
 
+	printf   ("main: 59: '%---34c%-107.*u'\n" ,7,-168,2003619509u);
+	ft_printf("main: 60: '%---34c%-107.*u'\n" ,7,-168,2003619509u);
+	printf("\n");
+
+	printf   ("main: 61: '%-190.*u%--6.26X%156.96s'\n" ,-109,3830289361u,1359348041u,"p@.CbB<\\?d");
+	ft_printf("main: 62: '%-190.*u%--6.26X%156.96s'\n" ,-109,3830289361u,1359348041u,"p@.CbB<\\?d");
+	printf("\n");
+
+	printf("\n");
+
 	printf   ("{%05.*d}\n", -15, 42);
 	ft_printf("{%05.*d}\n", -15, 42);
 	printf("\n");
@@ -36,28 +46,17 @@ int main()
 	ft_printf("{%.*s}\n", -5, "42");
 	printf("\n");
 
-
-
-	printf   ("{%3c}\n", 0);
-	ft_printf("{%3c}\n", 0);
-	printf("\n");
-
-	printf   ("{%*c}\n", 0, 0);
-	ft_printf("{%*c}\n", 0, 0);
-	printf("\n");
-
-	printf   ("{%*c}\n", -15, 0);
-	ft_printf("{%*c}\n", -15, 0);
-	printf("\n");
-
-	printf   ("main: 59: '%---34c%-107.*u'\n" ,7,-168,2003619509u);
-	ft_printf("main: 60: '%---34c%-107.*u'\n" ,7,-168,2003619509u);
-	printf("\n");
-
-	printf   ("main: 61: '%-190.*u%--6.26X%156.96s'\n" ,-109,3830289361u,1359348041u,"p@.CbB<\\?d");
-	ft_printf("main: 62: '%-190.*u%--6.26X%156.96s'\n" ,-109,3830289361u,1359348041u,"p@.CbB<\\?d");
-	printf("\n");
-
+//	printf   ("{%3c}\n", 0);
+//	ft_printf("{%3c}\n", 0);
+//	printf("\n");
+//	
+//	printf   ("{%*c}\n", 0, 0);
+//	ft_printf("{%*c}\n", 0, 0);
+//	printf("\n");
+//	
+//	printf   ("{%*c}\n", -15, 0);
+//	ft_printf("{%*c}\n", -15, 0);
+//	printf("\n");
 
 	return (0);
 }
