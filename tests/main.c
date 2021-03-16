4bfa9bec85412e2cb87b1af77c93407623ab2940
gabriel <gabriel80546@gmail.com>
2021-03-16 10:49:43 -0300

    *

--- tests/main.c
+++ tests/main.c
@@ -24,44 +24,14 @@ int main()
 	verbose = 0;
 	verbose = 1;
 
-	printf   ("main: 27: '%-5c'\n", '\0');
-	ft_printf("main: 27: '%-5c'\n", '\0');
-	printf   ("main: 28: '{%3c}'\n", 0);
-	ft_printf("main: 28: '{%3c}'\n", 0);
-
-
-
-	printf   ("%*d\n", 5, 42);
-	ft_printf("%*d\n", 5, 42);
-	printf("\n");
-
-	printf   ("{%*d}\n", -5, 42);
-	ft_printf("{%*d}\n", -5, 42);
-	printf("\n");
-
-	printf   ("{%*d}\n", 0, 42);
-	ft_printf("{%*d}\n", 0, 42);
-	printf("\n");
-
-	printf   ("{%*c}\n", 0, 0);
-	ft_printf("{%*c}\n", 0, 0);
-	printf("\n");
-
-	printf   ("{%*c}\n", -15, 0);
-	ft_printf("{%*c}\n", -15, 0);
-	printf("\n");
-
-	printf   ("{%.*d}\n", 5, 42);
-	ft_printf("{%.*d}\n", 5, 42);
+	printf   ("{%05.*d}\n", -15, 42);
+	ft_printf("{%05.*d}\n", -15, 42);
 	printf("\n");
 
 	printf   ("{%.*d}\n", -5, 42);
 	ft_printf("{%.*d}\n", -5, 42);
 	printf("\n");
 
-	printf   ("{%.*d}\n", 0, 42);
-	ft_printf("{%.*d}\n", 0, 42);
-	printf("\n");
 
 	return (0);
 }
