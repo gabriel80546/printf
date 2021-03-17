2a8dde9dcdc5566d360cda049d5de72156a64809
gabriel <gabriel80546@gmail.com>
2021-03-17 11:51:17 -0300

    revertendo para tag v0.1.0

--- tests/main.c
+++ tests/main.c
@@ -19,35 +19,6 @@
 
 int main()
 {
-
-	int		a = -4;
-	int		b = 0;
-	char	c = 'a';
-	int		d = 2147483647;
-	int		e = -2147483648;
-	int		f = 42;
-	int		g = 25;
-	int		h = 4200;
-	int		i = 8;
-	int		j = -12;
-	int		k = 123456789;
-	int		l = 0;
-	int		m = -12345678;
-	char	*n = "abcdefghijklmnop";
-	char	*o = "-a";
-	char	*p = "-12";
-	char	*q = "0";
-	char	*r = "%%";
-	char	*s = "-2147483648";
-	char	*t = "0x12345678";
-	char	*u = "-0";
-
-
-
-
-
-
-
 	printf("*******************************************************\n");
 	printf("************************* main ************************\n");
 	printf("*******************************************************\n");
@@ -76,25 +47,25 @@ int main()
 	printf("\n");
 
 
-	ft_printf("main: 50: '%15p'\n", NULL);
 	printf   ("main: 50: '            0x0'\n");
 	ft_printf("main: 50: '%15p'\n", NULL);
 	printf("\n");
 
-	ft_printf("main: 50: '%-15p'\n", NULL);
 	printf   ("main: 50: '0x0            '\n");
 	ft_printf("main: 50: '%-15p'\n", NULL);
 	printf("\n");
 
 
 
-	while (a < 5) //T34-69
-	{
-		printf   ("a = %d\n", a);
-		printf   ("%.*i, %.*d, %.*d, %.*d, %.*d, %.*d, %.*d, %.*d\n", a, i, a, j, a, k, a, l, a, m, a, c, a, e, a, d);
-		ft_printf("%.*i, %.*d, %.*d, %.*d, %.*d, %.*d, %.*d, %.*d\n", a, i, a, j, a, k, a, l, a, m, a, c, a, e, a, d);
-		a++;
-	}
+
+
+
+
+
+
+
+
+
 
 
 	return (0);
