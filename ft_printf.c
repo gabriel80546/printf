f075a58683dead2fab6e66c1177999f53f33b8c2
Gabriel <gabriel80546@gmail.com>
2021-03-18 11:37:22 -0300

    testando hex

--- ft_printf.c
+++ ft_printf.c
@@ -1165,6 +1165,107 @@ t_print	get_pointer(t_print print, int *counter, va_list args)
 
 t_print	get_pointer(t_print print, int *counter, va_list args)
 {
+	t_print	saida;
+	char	*temp;
+	unsigned long		n;
+	int		i;
+	int		tamanho;
+	int		tt;
+
+
+	saida = print;
+	n = va_arg(args, unsigned long);
+
+	tt = 0;
+	if (saida.flags.n_right == 0 && saida.flags.n_left == 0 && saida.flags.right_asteristic == 1 && n == 0)
+		tt = 1;
+
+	if (saida.flags.n_right < 0)
+	{
+		if ((saida.flags.pad_zeros == 1 && saida.flags.minus != 1) && saida.flags.n_left_indf == 0)
+			saida.flags.n_right = saida.flags.n_left;
+	}
+
+	if (saida.flags.n_left >= 0 && saida.flags.minus == 0)
+	{
+		i = 0;
+		tamanho = ft_itoa_x_ul_log((long)n) - 1;
+		if ((tamanho - saida.flags.n_right) < 0)
+			tamanho -= (tamanho - saida.flags.n_right);
+		while (i < (saida.flags.n_left - tamanho))
+		{
+			if ((saida.flags.pad_zeros == 1 && saida.flags.minus != 1))
+				if (saida.flags.precision == 1)
+					ft_pchar(' ', counter);
+				else
+					ft_pchar('0', counter);
+			else
+				ft_pchar(' ', counter);
+			i++;
+		}
+	}
+
+	if (saida.flags.precision == 1 && saida.flags.n_right >= 0)
+	{
+		i = 0;
+		tamanho = ft_itoa_x_ul_log((long)n) - 1;
+		ft_pnchar('0', saida.flags.n_right - tamanho, counter);
+	}
+
+	if (tt == 1)
+	{
+		temp = ft_calloc(1, 1);
+	}
+	else
+	{
+		if (n == 0 && saida.flags.precision == 1 && (saida.flags.pad_zeros == 0 || saida.flags.minus == 1))
+		{
+			temp = ft_calloc(1, 2);
+			if (saida.flags.n_right > 0 || saida.flags.n_right < 0)
+				temp[0] = '0';
+			else if (saida.flags.n_left > 0)
+				temp[0] = ' ';
+			ft_pstr(temp, counter);
+		}
+		else
+		{
+			if (n == 0 && saida.flags.n_left_indf == 0 && saida.flags.n_right_indf == 0 && saida.flags.n_left != 0 && saida.flags.n_right == 0)
+			{
+				temp = ft_calloc(1, 1);
+				ft_pchar(' ', counter);
+			}
+			else
+			{
+				//temp = ft_itoa(n);
+				//temp = ft_itoa_ui(n);
+				//temp = ft_itoa_x(n);
+				//ft_pstr(temp, counter);
+
+				ft_pstr("0x", counter);
+				temp = ft_itoa_x_ul(n);
+				if (!(saida.flags.precision == 1 && n == 0))
+					ft_pstr(temp, counter);
+			}
+		}
+	}
+
+	if (saida.flags.n_left >= 0 && saida.flags.minus == 1)
+	{
+		i = 0;
+		tamanho = ft_itoa_x_ul_log((long)n) - 1;
+		if ((tamanho - saida.flags.n_right) < 0)
+			tamanho -= (tamanho - saida.flags.n_right);
+		ft_pnchar(' ',saida.flags.n_left - tamanho, counter); 
+	}
+
+	free(temp);
+	saida.estado = UNTIL_PERCENT;
+	return (saida);
+}
+
+/*
+t_print	get_pointer(t_print print, int *counter, va_list args)
+{
 	t_print			saida;
 	char			*temp;
 	int				tamanho;
@@ -1174,14 +1275,6 @@ t_print	get_pointer(t_print print, int *counter, va_list args)
 	int				i;
 
 	saida = print;
-
-	//flag = 0;
-	/*
-	if (saida.flags.n_right == -1)
-		saida.flags.n_right = va_arg(args, int);
-	//else if (saida.flags.n_right == -2)
-	*/
-	//	flag = 1;
 
 	n = va_arg(args, unsigned long);
 	if (debug > 2) { logging("parte1\n"); }
@@ -1255,6 +1348,7 @@ t_print	get_pointer(t_print print, int *counter, va_list args)
 	if (debug > 2) { logging("*counter = %d\n", *counter); }
 	return (saida);
 }
+*/
 
 t_print	get_percent(t_print print, int *counter, va_list args)
 {
