814968ff80e8582576a60b3989360e81bc974dc0
gabriel <gabriel80546@gmail.com>
2021-03-19 20:58:44 -0300

    get_char

--- get_char.c
+++ get_char.c
@@ -16,38 +16,17 @@ t_print	get_char(t_print print, int *counter, va_list args)
 {
 	t_print	saida;
 	char	temp;
-	int		i;
 
 	saida = print;
 	if (saida.flags.n_left >= 0 && saida.flags.minus == 0)
-	{
-		i = 0;
-		while (i < (saida.flags.n_left - 1))
-		{
-			if (saida.flags.pad_zeros == 1)
-				ft_pchar('0', counter);
-			else
-				ft_pchar(' ', counter);
-			i++;
-		}
-	}
+		if (saida.flags.pad_zeros == 1)
+			ft_pnchar('0', saida.flags.n_left - 1, counter);
+		else
+			ft_pnchar(' ', saida.flags.n_left - 1, counter);
 	temp = va_arg(args, int);
-	if (temp == 0 && 0)
-	{
-		saida.estado = END;
-		return (saida);
-	}
-	else
-		ft_pchar(temp, counter);
+	ft_pchar(temp, counter);
 	if (saida.flags.n_left >= 0 && saida.flags.minus == 1)
-	{
-		i = 0;
-		while (i < (saida.flags.n_left - 1))
-		{
-			ft_pchar(' ', counter);
-			i++;
-		}
-	}
+		ft_pnchar(' ', saida.flags.n_left - 1, counter);
 	saida.estado = UNTIL_PERCENT;
 	return (saida);
 }
