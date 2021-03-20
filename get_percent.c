17cb883acd03dc44b5a406ddcdadb3b6700d3fd1
gabriel <gabriel80546@gmail.com>
2021-03-19 21:17:51 -0300

    get_percent

--- get_percent.c
+++ get_percent.c
@@ -18,34 +18,16 @@ t_print	get_percent(t_print print, int *counter)
 	int		i;
 
 	saida = print;
-	/*
-	if (saida.flags.n_right == -1)
-	{
-		saida.flags.n_right = va_arg(args, int);
-	}
-	*/
 	if (saida.flags.n_left >= 0 && saida.flags.minus == 0)
 	{
-		i = 0;
-		while (i < (saida.flags.n_left - 1))
-		{
-			if (saida.flags.pad_zeros == 1)
-				ft_pchar('0', counter);
-			else
-				ft_pchar(' ', counter);
-			i++;
-		}
+		if (saida.flags.pad_zeros == 1)
+			ft_pnchar('0', saida.flags.n_left - 1, counter);
+		else
+			ft_pnchar(' ', saida.flags.n_left - 1, counter);
 	}
 	ft_pchar('%', counter);
 	if (saida.flags.n_left >= 0 && saida.flags.minus == 1)
-	{
-		i = 0;
-		while (i < (saida.flags.n_left - 1))
-		{
-			ft_pchar(' ', counter);
-			i++;
-		}
-	}
+		ft_pnchar(' ', saida.flags.n_left - 1, counter);
 	saida.estado = UNTIL_PERCENT;
 	return (saida);
 }
